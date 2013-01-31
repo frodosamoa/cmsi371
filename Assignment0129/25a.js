cc050f01412e0ce010d23dffbd2971613cbebaf1
frodosamoa <andrewrkowalczyk@gmail.com>
2013-01-31 11:32:40 -0800

    Removed old homework folder

--- Assignment0129/25a.js
+++
@@ -1,9 +0,0 @@
-(function () {
-    //declare all the variables
-    var canvas = document.getElementById("canvas"),
-        renderingContext = canvas.getContext("2d");
-     
-    // makes the blue square 
-    renderingContext.fillStyle = "blue";      
-    renderingContext.fillRect(100, 100, 100, 100);
-}());
