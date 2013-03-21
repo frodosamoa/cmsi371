4ab3acf52d66f333a815f9359ab9f986489c9c9a
dondi <dondi@lmu.edu>
2013-03-21 16:53:36 -0700

    Head start on matrix library.

---
+++ homework/matrix4x4/matrix4x4-test.js
@@ -0,0 +1,198 @@
+/*
+ * Unit tests for our Matrix4x4 object.
+ */
+$(function () {
+
+    // This suite checks instantiation basics.
+    test("Creation and Data Access", function () {
+        var m1 = new Matrix4x4();
+        deepEqual(m1.elements,
+            [1, 0, 0, 0,
+             0, 1, 0, 0,
+             0, 0, 1, 0,
+             0, 0, 0, 1],
+            "Default matrix constructor");
+    });
+
+    test("Pure Transformation Matrices", function () {
+        var m = Matrix4x4.getTranslationMatrix(5, 9, -1);
+        deepEqual(m.elements,
+            [1, 0, 0, 5,
+             0, 1, 0, 9,
+             0, 0, 1, -1,
+             0, 0, 0, 1],
+            "Pure translation matrix");
+
+        m = Matrix4x4.getScaleMatrix(2, 5, 21);
+        deepEqual(m.elements,
+            [2, 0,  0, 0,
+             0, 5,  0, 0,
+             0, 0, 21, 0,
+             0, 0,  0, 1],
+            "Pure scale matrix");
+
+        m = Matrix4x4.getRotationMatrix(Math.PI / 6, 0, 0, 1);
+        deepEqual(m.elements,
+            [Math.cos(Math.PI / 6),                  -0.5, 0, 0,
+                               0.5, Math.cos(Math.PI / 6), 0, 0,
+                                 0,                     0, 1, 0,
+                                 0,                     0, 0, 1],
+            "Rotation by 30 degrees about the z-axis");
+    });
+
+/*
+    test("Addition and Subtraction", function () {
+        var v1 = new Vector(4, 5),
+            v2 = new Vector(-10, 4),
+            vresult = v1.add(v2);
+
+        equal(vresult.dimensions(), 2, "Vector sum size check");
+        equal(vresult.x(), -6, "Vector sum first element");
+        equal(vresult.y(), 9, "Vector sum second element");
+
+        v1 = new Vector(0, -2, 3, 5);
+        v2 = new Vector(-2, 1, 0, 7);
+        vresult = v1.subtract(v2);
+        equal(vresult.dimensions(), 4, "Vector difference size check");
+        equal(vresult.x(), 2, "Vector difference first element");
+        equal(vresult.y(), -3, "Vector difference second element");
+        equal(vresult.z(), 3, "Vector difference third element");
+        equal(vresult.w(), -2, "Vector difference fourth element");
+
+        // Check for errors.
+        v1 = new Vector(5, 8, 10, 2);
+        v2 = new Vector(1, 2, 2);
+
+        // We can actually check for a *specific* exception, but
+        // we won't go that far for now.
+        raises(
+            function () {
+                return v1.add(v2);
+            },
+            "Check for vectors of different sizes"
+        );
+    });
+
+    test("Scalar Multiplication and Division", function () {
+        var v = new Vector(8, 2, 3),
+            vresult = v.multiply(2);
+
+        equal(vresult.x(), 16, "Vector scalar multiplication first element");
+        equal(vresult.y(), 4, "Vector scalar multiplication second element");
+        equal(vresult.z(), 6, "Vector scalar multiplication third element");
+
+        vresult = vresult.divide(4);
+
+        equal(vresult.x(), 4, "Vector scalar division first element");
+        equal(vresult.y(), 1, "Vector scalar division second element");
+        equal(vresult.z(), 1.5, "Vector scalar division third element");
+    });
+
+    test("Dot Product", function () {
+        var v1 = new Vector(-5, -2),
+            v2 = new Vector(-3, 4);
+
+        equal(v1.dot(v2), 7, "2D dot product");
+
+        // Try for a perpendicular.
+        v1 = new Vector(Math.sqrt(2) / 2, Math.sqrt(2) / 2);
+        v2 = new Vector(-Math.sqrt(2) / 2, Math.sqrt(2) / 2);
+        equal(v1.dot(v2), 0, "Perpendicular 2D dot product");
+
+        // Try 3D.
+        v1 = new Vector(3, 2, 5);
+        v2 = new Vector(4, -1, 3);
+        equal(v1.dot(v2), 25, "3D dot product");
+
+        // Check for errors.
+        v1 = new Vector(4, 2);
+        v2 = new Vector(3, 9, 1);
+
+        // We can actually check for a *specific* exception, but
+        // we won't go that far for now.
+        raises(
+            function () {
+                return v1.dot(v2);
+            },
+            "Check for vectors of different sizes"
+        );
+    });
+
+    test("Cross Product", function () {
+        var v1 = new Vector(3, 4),
+            v2 = new Vector(1, 2),
+            vresult;
+
+        // The cross product is restricted to 3D, so we start
+        // with an error check.
+        raises(
+            function () {
+                return v1.cross(v2);
+            },
+            "Check for non-3D vectors"
+        );
+
+        // Yeah, this is a bit of a trivial case.  But it at least
+        // establishes the right-handedness of a cross-product.
+        v1 = new Vector(1, 0, 0);
+        v2 = new Vector(0, 1, 0);
+        vresult = v1.cross(v2);
+
+        equal(vresult.x(), 0, "Cross product first element");
+        equal(vresult.y(), 0, "Cross product second element");
+        equal(vresult.z(), 1, "Cross product third element");
+
+        // This one shows that switching vector order produces
+        // the opposite-pointing normal.
+        vresult = v2.cross(v1);
+
+        equal(vresult.x(), 0, "Cross product first element");
+        equal(vresult.y(), 0, "Cross product second element");
+        equal(vresult.z(), -1, "Cross product third element");
+    });
+
+    test("Magnitude and Unit Vectors", function () {
+        var v = new Vector(3, 4);
+
+        // The classic example.
+        equal(v.magnitude(), 5, "2D magnitude check");
+
+        // Kind of a cheat, but still tests the third dimension.
+        v = new Vector(5, 0, 12);
+        equal(v.magnitude(), 13, "3D magnitude check");
+
+        // Now for unit vectors.
+        v = (new Vector(3, 4)).unit();
+
+        equal(v.magnitude(), 1, "2D unit vector check");
+        equal(v.x(), 3 / 5, "2D unit vector first element");
+        equal(v.y(), 4 / 5, "2D unit vector second element");
+
+        v = (new Vector(0, -7, 24)).unit();
+
+        equal(v.magnitude(), 1, "3D unit vector check");
+        equal(v.x(), 0, "3D unit vector first element");
+        equal(v.y(), -7 / 25, "3D unit vector second element");
+        equal(v.z(), 24 / 25, "3D unit vector third element");
+    });
+
+    test("Projection", function () {
+        var v = new Vector(3, 3, 0),
+            vresult = v.projection(new Vector(5, 0, 0));
+
+        equal(vresult.magnitude(), 3, "3D vector projection magnitude check");
+        equal(vresult.x(), 3, "3D vector projection first element");
+        equal(vresult.y(), 0, "3D vector projection second element");
+        equal(vresult.z(), 0, "3D vector projection third element");
+
+        // Error check: projection only applies to vectors with the same
+        // number of dimensions.
+        raises(
+            function () {
+                (new Vector(5, 2)).projection(new Vector(9, 8, 1));
+            },
+            "Ensure that projection applies only to vectors with the same number of dimensions"
+        );
+    });
+*/
+});
---
+++ homework/matrix4x4/matrix4x4.js
@@ -0,0 +1,211 @@
+var Matrix4x4 = (function () {
+    // Define the constructor.
+    var matrix4x4 = function () {
+        this.elements = arguments.length ?
+            [].slice.call(arguments) :
+
+            [1, 0, 0, 0,
+             0, 1, 0, 0,
+             0, 0, 1, 0,
+             0, 0, 0, 1];
+    };
+
+    matrix4x4.getTranslationMatrix = function (tx, ty, tz) {
+        return new Matrix4x4(
+            1, 0, 0, tx,
+            0, 1, 0, ty,
+            0, 0, 1, tz,
+            0, 0, 0, 1
+        );
+    };
+
+    matrix4x4.getScaleMatrix = function (sx, sy, sz) {
+        return new Matrix4x4(
+            sx,  0,  0, 0,
+             0, sy,  0, 0,
+             0,  0, sz, 0,
+             0,  0,  0, 1
+        );
+    };
+
+    matrix4x4.getRotationMatrix = function (angle, x, y, z) {
+        // In production code, this function should be associated
+        // with a matrix object with associated functions.
+        var axisLength = Math.sqrt((x * x) + (y * y) + (z * z)),
+            s = Math.sin(angle * Math.PI / 180.0),
+            c = Math.cos(angle * Math.PI / 180.0),
+            oneMinusC = 1.0 - c,
+
+            // We can't calculate this until we have normalized
+            // the axis vector of rotation.
+            x2, // "2" for "squared."
+            y2,
+            z2,
+            xy,
+            yz,
+            xz,
+            xs,
+            ys,
+            zs;
+
+        // Normalize the axis vector of rotation.
+        x /= axisLength;
+        y /= axisLength;
+        z /= axisLength;
+
+        // *Now* we can calculate the other terms.
+        x2 = x * x;
+        y2 = y * y;
+        z2 = z * z;
+        xy = x * y;
+        yz = y * z;
+        xz = x * z;
+        xs = x * s;
+        ys = y * s;
+        zs = z * s;
+
+        // GL expects its matrices in column major order.
+        return new Matrix4x4(
+            (x2 * oneMinusC) + c, (xy * oneMinusC) - zs, (xz * oneMinusC) + ys, 0.0,
+            (xy * oneMinusC) + zs, (y2 * oneMinusC) + c, (yz * oneMinusC) - xs, 0.0,
+            (xz * oneMinusC) - ys, (yz * oneMinusC) + xs, (z2 * oneMinusC) + c, 0.0,
+            0.0, 0.0, 0.0, 1.0
+        );
+    };
+
+/*
+    // Basic methods.
+    vector.prototype.dimensions = function () {
+        return this.elements.length;
+    };
+
+    vector.prototype.x = function () {
+        return this.elements[0];
+    };
+
+    vector.prototype.y = function () {
+        return this.elements[1];
+    };
+
+    vector.prototype.z = function () {
+        return this.elements[2];
+    };
+
+    vector.prototype.w = function () {
+        return this.elements[3];
+    };
+
+    // Addition and subtraction.
+    vector.prototype.add = function (v) {
+        var result = new Vector(),
+            i,
+            max;
+
+        // Dimensionality check.
+        checkDimensions(this, v);
+
+        for (i = 0, max = this.dimensions(); i < max; i += 1) {
+            result.elements[i] = this.elements[i] + v.elements[i];
+        }
+
+        return result;
+    };
+
+    vector.prototype.subtract = function (v) {
+        var result = new Vector(),
+            i,
+            max;
+
+        // Dimensionality check.
+        checkDimensions(this, v);
+
+        for (i = 0, max = this.dimensions(); i < max; i += 1) {
+            result.elements[i] = this.elements[i] - v.elements[i];
+        }
+
+        return result;
+    };
+
+    // Scalar multiplication and division.
+    vector.prototype.multiply = function (s) {
+        var result = new Vector(),
+            i,
+            max;
+
+        for (i = 0, max = this.dimensions(); i < max; i += 1) {
+            result.elements[i] = this.elements[i] * s;
+        }
+
+        return result;
+    };
+
+    vector.prototype.divide = function (s) {
+        var result = new Vector(),
+            i,
+            max;
+
+        for (i = 0, max = this.dimensions(); i < max; i += 1) {
+            result.elements[i] = this.elements[i] / s;
+        }
+
+        return result;
+    };
+
+    // Dot product.
+    vector.prototype.dot = function (v) {
+        var result = 0,
+            i,
+            max;
+
+        // Dimensionality check.
+        checkDimensions(this, v);
+
+        for (i = 0, max = this.dimensions(); i < max; i += 1) {
+            result += this.elements[i] * v.elements[i];
+        }
+
+        return result;
+    };
+
+    // Cross product.
+    vector.prototype.cross = function (v) {
+        // This method is for 3D vectors only.
+        if (this.dimensions() !== 3 || v.dimensions() !== 3) {
+            throw "Cross product is for 3D vectors only.";
+        }
+
+        // With 3D vectors, we can just return the result directly.
+        return new Vector(
+            (this.y() * v.z()) - (this.z() * v.y()),
+            (this.z() * v.x()) - (this.x() * v.z()),
+            (this.x() * v.y()) - (this.y() * v.x())
+        );
+    };
+
+    // Magnitude and unit vector.
+    vector.prototype.magnitude = function () {
+        // Make use of the dot product.
+        return Math.sqrt(this.dot(this));
+    };
+
+    vector.prototype.unit = function () {
+        // At this point, we can leverage our more "primitive" methods.
+        return this.divide(this.magnitude());
+    };
+
+    // Projection.
+    vector.prototype.projection = function (v) {
+        var unitv;
+
+        // Dimensionality check.
+        checkDimensions(this, v);
+
+        // Plug and chug :)
+        // The projection of u onto v is u dot the unit vector of v
+        // times the unit vector of v.
+        unitv = v.unit();
+        return unitv.multiply(this.dot(unitv));
+    };
+*/
+    return matrix4x4;
+})();
