a90a6cbb2669ff07bf9566d3d3b699b2b4fb10c5
frodosamoa <andrewrkowalczyk@gmail.com>
2013-03-29 22:05:01 -0700

    commented out shapes and edited sphere code

--- homework/pipeline/hello-webgl-again.js
+++ homework/pipeline/hello-webgl-again.js
@@ -127,7 +127,7 @@
 
     // Build the objects to display.
     objectsToDraw = [
-        {
+        /*{
             color: { r: 0.0, g: 0.5, b: 0.0 },
             vertices: Shapes.toRawLineArray(Shapes.cube()),
             mode: gl.LINES
@@ -143,12 +143,12 @@
             color: { r: 0.0, g: 0.5, b: 0.0 },
             vertices: Shapes.toRawLineArray(Shapes.cross()),
             mode: gl.LINES
-        },
+        },*/
 
         {
             color: { r: 1.0, g: 0.0, b: 0.0 },
-            vertices: Shapes.toRawLineArray(Shapes.sphere()),
-            mode: gl.LINES
+            vertices: Shapes.toRawTriangleArray(Shapes.sphere()),
+            mode: gl.TRIANGLES
         } 
     ];
 
